fb4dc5963d79b2353775986bb4f8d1bfbdf63358
imadisimran <imadisimran@gmail.com>
2025-11-22 13:07:29 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app=express()
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("Server is working")
+})
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.xc8a26e.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const eco_track=client.db('eco_track')
+    const challenges=eco_track.collection('challenges')
+    app.get('/challenges',async(req,res)=>{
+      const cursor=challenges.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running at ${port}`)
+})
