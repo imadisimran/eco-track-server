fb236f3eb773de4069ad97e855cfd237e3718b6f
imadisimran <imadisimran@gmail.com>
2025-11-22 16:32:54 +0600

    all challenges

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
+    app.get('/challenges',async(req,res)=>{
+      const cursor=challenges.find().sort({startDate:-1})
+      const result = await cursor.toArray()
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
