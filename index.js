98ed42ed40e5eba0a795ea65a35c5d93bf383dec
imadisimran <imadisimran@gmail.com>
2025-11-23 20:41:30 +0600

    user challenge apis

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app=express()
+const app = express()
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send("Server is working")
+app.get('/', (req, res) => {
+  res.send("Server is working")
 })
 
 
@@ -28,70 +28,93 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const eco_track=client.db('eco_track')
-    const challenges=eco_track.collection('challenges')
-    const tips=eco_track.collection('tips')
-    const events=eco_track.collection('events')
+    const eco_track = client.db('eco_track')
+    const challenges = eco_track.collection('challenges')
+    const tips = eco_track.collection('tips')
+    const events = eco_track.collection('events')
+    const usersChallenge = eco_track.collection('usersChallenge')
 
     //Get Api
-    app.get('/recent-challenges',async(req,res)=>{
-      const cursor=challenges.find().sort({endDate:-1}).limit(5)
+    app.get('/recent-challenges', async (req, res) => {
+      const cursor = challenges.find().sort({ endDate: -1 }).limit(5)
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.get('/challenges',async(req,res)=>{
-      const cursor=challenges.find().sort({startDate:-1})
+    app.get('/challenges', async (req, res) => {
+      const cursor = challenges.find().sort({ startDate: -1 })
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.get('/challenges/:id',async (req,res)=>{
-      const id=req.params.id
-      const query={_id:new ObjectId(id)}
+    app.get('/challenges/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
       const result = await challenges.findOne(query)
-      if(!result){
-        return res.send({message:'Not Found'})
+      if (!result) {
+        return res.send({ message: 'Not Found' })
       }
       res.send(result)
     })
 
-    app.get('/recent-tips',async(req,res)=>{
-      const cursor=tips.find().sort({createdAt:-1}).limit(5)
+    app.get('/recent-tips', async (req, res) => {
+      const cursor = tips.find().sort({ createdAt: -1 }).limit(5)
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.get('/events',async(req,res)=>{
+    app.get('/events', async (req, res) => {
       const cursor = events.find()
-      const result=await cursor.toArray()
+      const result = await cursor.toArray()
       res.send(result)
     })
 
     //Post Api
-    app.post('/challenges',async(req,res)=>{
+    app.post('/challenges', async (req, res) => {
       const newChallenge = req.body
       const result = await challenges.insertOne(newChallenge)
       res.send(result)
     })
 
+    app.post('/add-user-challenge', async (req, res) => {
+      const info = req.body
+      const result = await usersChallenge.insertOne(info)
+      res.send(result)
+    })
+
+    app.post('/check-user-challenge', async (req, res) => {
+      const query = req.body
+      const count = await usersChallenge.countDocuments(query, { limit: 1 })
+      res.send(count)
+    })
+
     //Delete Api
-    app.delete('/challenges/:id',async(req,res)=>{
+    app.delete('/challenges/:id', async (req, res) => {
       const id = req.params.id
-      const query={_id:new ObjectId(id)}
-      const result=await challenges.deleteOne(query)
+      const query = { _id: new ObjectId(id) }
+      const result = await challenges.deleteOne(query)
       res.send(result)
     })
 
     //Update Api
-    app.patch('/challenges/:id',async(req,res)=>{
-      const id=req.params.id
-      const updatedChallenge=req.body
-      const update={
+    app.patch('/challenges/:id', async (req, res) => {
+      const id = req.params.id
+      const updatedChallenge = req.body
+      const update = {
         $set: updatedChallenge
       }
-      const query={_id:new ObjectId(id)}
-      const result = await challenges.updateOne(query,update)
+      const query = { _id: new ObjectId(id) }
+      const result = await challenges.updateOne(query, update)
+      res.send(result)
+    })
+
+    app.patch('/challenges/join/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const update = {
+        $inc: { participants: 1 }
+      }
+      const result = await challenges.updateOne(query, update)
       res.send(result)
     })
 
@@ -107,6 +130,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port,()=>{
-    console.log(`server is running at ${port}`)
+app.listen(port, () => {
+  console.log(`server is running at ${port}`)
 })
